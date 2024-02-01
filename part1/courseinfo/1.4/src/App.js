27a90e3b473206bee857d3ba523cec729d9d8e70
Raulllllll <48178795+Raulllllll@users.noreply.github.com>
2024-02-01 22:56:44 +0200

    Update App.js

--- part1/courseinfo/1.4/src/App.js
+++ part1/courseinfo/1.4/src/App.js
@@ -17,7 +17,6 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  const num = 0
   return (
     <div>
       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
