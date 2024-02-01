c5ace57b19bb9fc094bdbadfb503c91ba18c7a1f
Raulllllll <48178795+Raulllllll@users.noreply.github.com>
2024-02-01 22:56:20 +0200

    Update App.js

--- part1/courseinfo/1.4/src/App.js
+++ part1/courseinfo/1.4/src/App.js
@@ -9,15 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.name} {props.exercise}</p>
+      {props.parts.map(part => 
+        <p>{part.name} {part.exercises}</p>
+      )}
     </div>
   )
 }
 
 const Total = (props) => {
+  const num = 0
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
@@ -42,10 +45,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content name={parts[0].name} exercise={parts[0].exercises} />
-      <Content name={parts[1].name} exercise={parts[1].exercises} />
-      <Content name={parts[2].name} exercise={parts[2].exercises} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
