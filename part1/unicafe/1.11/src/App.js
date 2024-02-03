e1a30b0d5acbfe97919093d1ceb3e16a412ffa00
Raulllllll <48178795+Raulllllll@users.noreply.github.com>
2024-02-04 01:34:13 +0200

    Update App.js

--- part1/unicafe/1.11/src/App.js
+++ part1/unicafe/1.11/src/App.js
@@ -30,12 +30,14 @@ const Statistics = (props) => {
   return (
     <div>
       <table>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive+" %"} />
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive+" %"} />
+        </tbody>
       </table>
     </div>
   )
