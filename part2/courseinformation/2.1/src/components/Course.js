216240151e466658153bb828b25cceadcafb66d9
Raulllllll <48178795+Raulllllll@users.noreply.github.com>
2024-02-05 01:30:56 +0200

    Add files via upload

---
+++ part2/courseinformation/2.1/src/components/Course.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Part = ({name,exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    parts.map(part => <Part name={part.name} exercises={part.exercises} />)
+  )
+}
+
+const Header = ({header}) => {
+  return (
+    <h1>
+      {header}
+    </h1>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+
+
+
+
+
+
+export default Course
