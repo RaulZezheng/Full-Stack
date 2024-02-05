e98076be435af67d3f27f868753c89b4aef30cbe
Raulllllll <48178795+Raulllllll@users.noreply.github.com>
2024-02-05 15:00:57 +0200

    Add files via upload

---
+++ part2/courseinformation/2.2/src/components/Course.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Part = ({name,exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)
+  )
+}
+
+const Header = ({header}) => {
+  return (
+    <h1>
+      {header}
+    </h1>
+  )
+}
+
+const Accumulate = ({sum}) => {
+  return (
+    <p>
+      <b>
+        total of {sum} exercises
+      </b>
+    </p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+      <Accumulate sum={course.parts.reduce((acc,part) => acc+part.exercises,0)}/>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+export default Course
